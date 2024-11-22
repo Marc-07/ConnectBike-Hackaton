cd5b6d577fd8cbd45da2d2ab4635b71a00143424
Marc-07 <maanraca01@gmail.com>
2024-11-22 09:32:24 -0500

    agregar productlist

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,26 +1,53 @@
-import {useEffect, useState } from 'react'
-import fetchBikeStations from '../API/fetchBikeStations';
+import { useEffect, useState } from 'react';
+import "../styles/ProductList.css";
 
 const ProductList = () => {
+  const [stations, setStations] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const [stations, setStations] = useState ([]);
-    const [loading, setLoading] = useState (true)
-    const [error, setError] = useState (null);
+  // Datos de la API
+  useEffect(() => {
+    const fetchBikeStations = async () => {
+      try {
+        const response = await fetch("https://api.citybik.es/v2/networks/tembici-bogota");
+        if (!response.ok) {
+          throw new Error("Error al obtener las estaciones de Bogotá.");
+        }
+        const data = await response.json();
+        const limitedStations = data.network.stations.slice(0,12)
+        setStations(limitedStations);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    //Datos de la API
-    useEffect(()=>{
-        fetchBikeStations();
-    },[])
+    fetchBikeStations();
+  }, []);
 
-    if (loading) return <p>cargando</p>;
-    if (error) return <p>error cargando</p>
+  if (loading) return <p>Cargando...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
-    <div>
-      <h1>Estaciones Disponiblers</h1>
-        
+    <div className="container">
+      <h1 className="title">Estaciones Disponibles</h1>
+      <div className="cards-container">
+        {stations.map((station) => (
+          <div className="card" key={station.id}>
+            <h2 className="card-title">{station.name}</h2>
+            <p className="card-text">
+              <strong>Bicicletas disponibles:</strong> {station.free_bikes || 'N/A'}
+            </p>
+            <p className="card-text">
+              <strong>Dirección:</strong> {station.extra.address || 'No disponible'}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
